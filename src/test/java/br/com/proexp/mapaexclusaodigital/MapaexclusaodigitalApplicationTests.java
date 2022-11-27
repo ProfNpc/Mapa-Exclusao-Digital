a66281fb20dc533f570eaa79704ed04e74fb45a3
Alessandro <professornpc@gmail.com>
2022-11-27 20:39:31 -0300

    Versão inicial

---
+++ src/test/java/br/com/proexp/mapaexclusaodigital/MapaexclusaodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.proexp.mapaexclusaodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaexclusaodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
